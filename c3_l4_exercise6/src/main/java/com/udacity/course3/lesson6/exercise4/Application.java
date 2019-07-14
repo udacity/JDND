28ec15cbd43638caa13c8e5a20f6aefe14d61b18
Aravin Ramkumar <aravindan.ramkumar@gmail.com>
2019-07-13 22:35:43 -0700

    exercise 6

---
+++ c3_l4_exercise6/src/main/java/com/udacity/course3/lesson6/exercise4/Application.java
@@ -0,0 +1,26 @@
+package com.udacity.course3.lesson6.exercise4;
+
+public class Application {
+
+    public static void main(String[] args) {
+        // STEP 1: Craft the URI to connect to your local MongoDB server
+        // Host: localhost
+        // Port: 27017 (default)
+        // Username: course3
+        // Password: course3
+        // DB: jdnd-c3
+        String uri = "";
+
+        // STEP 2: Create a MongoClient
+
+
+        // STEP 3: Select the jdnd-c3 database to work with
+
+
+        // Perform all the steps listed in the exercise
+
+
+        // IMPORTANT: Make sure to close the MongoClient at the end so your program exits.
+    }
+
+}
