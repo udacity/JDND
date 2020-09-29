a210af01765799c7b60ad348d2f8ef3ed40cc32f
gajewskp <paula.gajewska-behnke@vimn.com>
2020-09-29 19:55:47 +0200

    - add cart controller integration tests

--- projects/P04-eCommerce Application/starter_code/src/test/java/com/example/demo/controllers/CartControllerIT.java
+++ projects/P04-eCommerce Application/starter_code/src/test/java/com/example/demo/controllers/CartControllerIT.java
@@ -16,7 +16,6 @@ import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -47,9 +46,6 @@ public class CartControllerIT {
 
     @Autowired
     private ItemRepository itemRepository;
-
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Test
     public void addTocart_http_ok() throws Exception {
